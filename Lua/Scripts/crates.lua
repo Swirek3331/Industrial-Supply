70d7ce53dc2644d48070014bc438c78e95e1a706
Krzysztof Siwy <krzysztofsiwy03@gmail.com>
2025-07-28 09:53:30 +0200

    Hooki; rzeczy się robią

--- Lua/Scripts/crates.lua
+++ Lua/Scripts/crates.lua
@@ -1,9 +1,272 @@
+-- Metal crates
+Hook.Add(
+    "IS.fulguriumcrate.spawn",
+    "IS.fulguriumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "fulgurium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.carboncrate.spawn",
+    "IS.carboncrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "carbon")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.coppercrate.spawn",
+    "IS.coppercrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "copper")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.ironcrate.spawn",
+    "IS.ironcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "iron")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.leadcrate.spawn",
+    "IS.leadcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "lead")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.siliconcrate.spawn",
+    "IS.siliconcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "silicon")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.thoriumcrate.spawn",
+    "IS.thoriumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "thorium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.tincrate.spawn",
+    "IS.tincrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "tin")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.titaniumcrate.spawn",
+    "IS.titaniumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "titanium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.uraniumcrate.spawn",
+    "IS.uraniumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "uranium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.zinccrate.spawn",
+    "IS.zinccrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "zinc")
+        end, 35)
+    end
+)
+
+-- Chemical crates
+Hook.Add(
+    "IS.calciumcrate.spawn",
+    "IS.calciumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "calcium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.chlorinecrate.spawn",
+    "IS.chlorinecrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "chlorine")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.lithiumcrate.spawn",
+    "IS.lithiumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "lithium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.magnesiumcrate.spawn",
+    "IS.magnesiumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "magnesium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.phosphoruscrate.spawn",
+    "IS.phosphoruscrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "phosphorus")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.potassiumcrate.spawn",
+    "IS.potassiumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "potassium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.sodiumcrate.spawn",
+    "IS.sodiumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "sodium")
+        end, 35)
+    end
+)
+
+-- Organic and special material crates
 Hook.Add(
     "IS.stabilozinecrate.spawn",
     "IS.stabilozinecrate.spawn",
     function(effect, deltaTime, item, targets, worldPosition, element)
         Timer.Wait(function()
             IS.Helpers.FillChest(item, "stabilozine")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.alienbloodcrate.spawn",
+    "IS.alienbloodcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "alienblood")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.abxcrate.spawn",
+    "IS.abxcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "antibiotics")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.elastincrate.spawn",
+    "IS.elastincrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "elastin")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.ethanolcrate.spawn",
+    "IS.ethanolcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "ethanol")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.opiumcrate.spawn",
+    "IS.opiumcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "opium")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.organicfibercrate.spawn",
+    "IS.organicfibercrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "organicfiber")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.paralyxiscrate.spawn",
+    "IS.paralyxiscrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "paralyxis")
+        end, 35)
+    end
+)
+
+Hook.Add(
+    "IS.sulphuricacidcrate.spawn",
+    "IS.sulphuricacidcrate.spawn",
+    function(effect, deltaTime, item, targets, worldPosition, element)
+        Timer.Wait(function()
+            IS.Helpers.FillChest(item, "sulphuricacid")
         end, 35)
     end
 )
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Here we are, at the gates of the mountain
 ## TODO
 
 - minleveldifficulty
+- ensure you cannot sell empty crates for hihg price or at all
+- adjust prices and avialability for all crates, based on material they hold
